0d1cfc11725494fdc28fe2cfa59a225e7235cb1b
jmstevenson1337 <jmstevenson918@gmail.com>
2025-07-08 16:04:20 -0500

    Added a call to `keys.py` if the key or encrypted password files do not exist.

--- crypto_test.py
+++ crypto_test.py
@@ -1,8 +1,24 @@
+from pathlib import Path
 from cryptography.fernet import Fernet
 import subprocess
 import time
+import os
 
-# Load key and encrypted password
+
+""" Path to the key and encrypted password files"""
+
+s_key_file = "secret.key"
+s_enc_file = "encrypted_password.bin"
+
+""" Check if the key and encrypted password files exist, if not, run keys.py to generate them"""
+
+if not Path(s_key_file).is_file() or not Path(s_enc_file).is_file():
+    print("Key or encrypted password file not found. Generating them...")
+    subprocess.run(["python", "keys.py"])
+else:
+    print("Key and encrypted password file found. Proceeding to decrypt the password.")
+
+""" Load key and encrypted password """
 
 with open("secret.key", "rb") as key_file:
 
